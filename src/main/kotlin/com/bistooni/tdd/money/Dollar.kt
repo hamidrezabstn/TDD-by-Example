f311211b7e98fd6aefe41a3e0b4a78524a4fb686
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2018-12-20 10:20:03 +0330

    chapter 4 -  Privacy

--- src/main/kotlin/com/bistooni/tdd/money/Dollar.kt
+++ src/main/kotlin/com/bistooni/tdd/money/Dollar.kt
@@ -1,6 +1,6 @@
 package com.bistooni.tdd.money
 
-class Dollar(val amount: Long) {
+class Dollar(private val amount: Long) {
 
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
